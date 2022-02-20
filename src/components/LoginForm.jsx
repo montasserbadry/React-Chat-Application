12afe5d058ba3527866a609790a08c2ffafcbe45
montasserbadry <muntasser.badry94@yahoo.com>
2022-02-21 01:25:57 +0200

    react-chat-application

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import axios from 'axios';
+import '../App.css';
+const projectID = '9a33e36c-adfd-4357-9fb2-3c28ba7097cb';
+
+const Modal = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const authObject = { 'Project-ID': projectID, 'User-Name': username, 'User-Secret': password };
+
+        try {
+            await axios.get('https://api.chatengine.io/chats', { headers: authObject });
+
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+
+            window.location.reload();
+            setError('');
+        } catch (err) {
+            setError('Oops, incorrect credentials.');
+        }
+    };
+
+    return (
+        <div className="wrapper">
+            <div className="form">
+                <h1 className="title">Chat Application</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="text" name="email" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
+                    <div align="center">
+                        <button type="submit" className="button">
+                            <span>Start chatting</span>
+                        </button>
+                    </div>
+                </form>
+                <h1>{error}</h1>
+            </div>
+        </div>
+
+    );
+};
+
+export default Modal;
